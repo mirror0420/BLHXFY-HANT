5a75ce7b76960d50670eeaaac3743d555d9a10ec
umisuna <blhx@danmu9.com>
2018-06-22 14:06:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## 碧藍幻想翻譯繁體數據
+這個 REPO 是 [碧藍幻想翻譯](https://github.com/biuuu/BLHXFY) 項目的繁體數據。
+
+初始内容來自簡體版，你可以在這裏提交繁體翻譯，或把已有的文本改爲繁體。
+
+## License
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
---
+++ packCsv.js
@@ -0,0 +1,139 @@
+const gulp = require('gulp')
+const md5File = require('md5-file')
+const del = require('del')
+const fs = require('fs-extra')
+const CSV = require('papaparse')
+const path = require('path')
+const ghpages = require('gh-pages')
+const zip = require('gulp-zip')
+
+const CSV_HOST = 'blhx-hant.danmu9.com'
+
+const through = require('through2')
+
+const scenarioMap = {}
+const skillMap = {}
+
+const writeFile = async (filePath, data, utf8bom = true) => {
+  return new Promise((rev, rej) => {
+    fs.ensureFileSync(filePath)
+
+    fs.writeFile(filePath, (utf8bom ? '\ufeff' : '') + data, (e) => {
+      if (e) rej(e)
+      rev()
+    })
+
+  })
+}
+
+const collectCsv = function(type) {
+  return through.obj(function(file, encoding, callback) {
+    if (file.isNull()) {
+        return callback(null, file);
+    }
+
+    if (file.isStream()) {
+        this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+    } else if (file.isBuffer()) {
+        const str = file.contents.toString()
+        const list = CSV.parse(str.replace(/^\ufeff/, ''), { header: true }).data
+        let newList = null
+        if (type === 'scenario') {
+          newList = list.map(item => {
+            if (item.id === 'info') {
+              scenarioMap[item.trans] = path.relative(path.resolve(__dirname, 'data/scenario/'), file.path).replace('\\', '/')
+            }
+            return item
+          })
+        } else if (type === 'skill') {
+          newList = list.map(item => {
+            if (item.id === 'npc') {
+              const idArr = item.detail.split('|')
+              if (idArr && idArr[0]) {
+                skillMap[idArr[0]] = path.relative(path.resolve(__dirname, 'data/skill/'), file.path).replace('\\', '/')
+              }
+            }
+            return item
+          })
+        }
+        const newStr = CSV.unparse(newList)
+        const newBuffer = Buffer.from(newStr)
+
+        file.contents = newBuffer
+        return callback(null, file)
+    }
+  })
+}
+
+gulp.task('clean:dist', function () {
+  return del([
+    './dist'
+  ])
+})
+
+gulp.task('move:static', ['clean:dist'], function () {
+  return gulp.src('./data/static/**/*')
+    .pipe(gulp.dest('./dist/blhxfy/data/static/'))
+})
+
+gulp.task('move:normalcsv', ['clean:dist'], function () {
+  return gulp.src('./data/*.csv')
+    .pipe(gulp.dest('./dist/blhxfy/data/'))
+})
+
+gulp.task('move:html', ['clean:dist'], function () {
+  return gulp.src('./data/*.html')
+    .pipe(gulp.dest('./dist/blhxfy/data/'))
+})
+
+gulp.task('move:scenario', ['clean:dist'], function () {
+  return gulp.src('./data/scenario/**/*.csv')
+    .pipe(collectCsv('scenario'))
+    .pipe(gulp.dest('./dist/blhxfy/data/scenario/'))
+})
+
+gulp.task('move:skill', ['clean:dist'], function () {
+  return gulp.src('./data/skill/**/*.csv')
+    .pipe(collectCsv('skill'))
+    .pipe(gulp.dest('./dist/blhxfy/data/skill/'))
+})
+
+gulp.task('scenarioMap', ['move:scenario'], function (done) {
+  fs.writeJson('./dist/blhxfy/data/scenario.json', scenarioMap, done)
+})
+
+gulp.task('skillMap', ['move:skill'], function (done) {
+  fs.writeJson('./dist/blhxfy/data/skill.json', skillMap, done)
+})
+
+gulp.task('pack', ['move:static', 'move:html', 'move:normalcsv', 'move:scenario', 'scenarioMap', 'cname'], function () {
+  // asar.createPackage('./dist/blhxfy/data/', './dist/blhxfy/data.asar', done)
+  return gulp.src('dist/blhxfy/data/**/*')
+    .pipe(zip('data.zip'))
+    .pipe(gulp.dest('dist/blhxfy/'))
+})
+
+gulp.task('md5', ['pack'], function (done) {
+  md5File('./dist/blhxfy/data.zip', (err, hash) => {
+    if (err) throw err
+    fs.copy('./dist/blhxfy/data.zip', `./dist/blhxfy/data.${hash.slice(0,5)}.zip`, () => {
+      fs.writeJson('./dist/blhxfy/manifest.json', { packname: `data.${hash.slice(0,5)}.zip` }, () => {
+        fs.remove('./dist/blhxfy/data.zip', done)
+      })
+    })
+  })
+})
+
+gulp.task('cname', ['clean:dist'], function () {
+  return writeFile('./dist/CNAME', CSV_HOST)
+})
+
+gulp.task('publish', ['md5'], function (done) {
+  ghpages.publish('dist', {
+    add: false
+  }, function () {
+    done()
+  })
+})
+
+gulp.task('default', ['move:static', 'move:normalcsv', 'move:scenario', 'move:skill', 'md5', 'pack', 'scenarioMap', 'skillMap', 'clean:dist', 'publish', 'cname']);
